abd1d8ee0deed215e841fab354623ea66cd11c46
chibuezemicahe <chibuezemicahe@gmail.com>
2025-02-18 14:53:44 +0100

    Fix: app not listening to right port

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ app.use("/", tickRoutes);
 
 const axios = require('axios');
 
-//Moved the interval function to the the tick function which can set the timeout
-// function keepAlive(){
-//     axios.get(process.env.BASE_URL)
-//         .then(() => console.log("Keeping service alive..."))
-//         .catch(err => console.error("Error in keep-alive ping:", err.message));
-// }
-// // Use setInterval to ping to keep the instance alive
-// setInterval(keepAlive, 5 * 60 * 1000); // Every 5 minutes
+// Moved the interval function to the the tick function which can set the timeout
+function keepAlive(){
+    axios.get(process.env.BASE_URL)
+        .then(() => console.log("Keeping service alive..."))
+        .catch(err => console.error("Error in keep-alive ping:", err.message));
+}
+// Use setInterval to ping to keep the instance alive
+setInterval(keepAlive, 5 * 60 * 1000); // Every 5 minutes
 
-const PORT=3000;
+const PORT=process.env.PORT|| 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
